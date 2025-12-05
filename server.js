02c0538c778201fe81ffe8b55127482eb137ab54
nightywolfy <nightywolfy@protonmail.com>
2025-12-05 05:21:03 -0800

    fix

--- server.js
+++ server.js
@@ -524,8 +524,8 @@ function createBot(nick, defaultTarget, options = {}) {
 
 
 const bots = {
-  dice1bot: createBot('dice1bot', 'dicebot'),
-  dice2bot: createBot('dice2bot', 'dicebot'),
+  dice1bot: createBot('dice1bot', 'rentobot'),
+  dice2bot: createBot('dice2bot', 'rentobot'),
   player1bot: createBot('player1bot', '##rento'),
   player2bot: createBot('player2bot', '##rento'),
   player3bot: createBot('player3bot', '##rento'),
