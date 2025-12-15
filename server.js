0880ad16dd193ac2875c4b05ef1571a8e6612b0a
nightywolfy <nightywolfy@protonmail.com>
2025-12-14 16:34:25 -0800

    irc

--- server.js
+++ server.js
@@ -325,7 +325,7 @@ let currentMap = 2;
 // ------------------------------------------------------------------
 function createBot(nick, defaultTarget, options = {}) {
   const client = new IRC.Client();
-  const host = options.host || 'irc.ipv6.libera.chat';
+  const host = options.host || 'irc.libera.chat';
   const port = options.port || 6667;
   const secure = !!options.secure;
   const nickServ = options.nickServ || null;
